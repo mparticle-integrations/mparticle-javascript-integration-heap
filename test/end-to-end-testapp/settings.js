55f9ed58d976881ba50310d9198b018ac87aad3d
SbDove <100377552+SbDove@users.noreply.github.com>
2024-05-28 13:36:30 -0700

    Update api key value for upload to github

--- test/end-to-end-testapp/settings.js
+++ test/end-to-end-testapp/settings.js
@@ -1,5 +1,5 @@
 var SDKsettings = {
-    apiKey: 'us1-1d7910fcadbf15449f3996edefbb9b60',
+    apiKey: 'TestApiKey',
     /* fill in SDKsettings with any particular settings or options your sdk requires in order to
     initialize, this may be apiKey, projectId, primaryCustomerType, etc. These are passed
     into the src/initialization.js file as the
