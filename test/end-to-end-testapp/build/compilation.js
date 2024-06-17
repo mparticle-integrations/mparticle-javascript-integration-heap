833bc852db530277aad85918dbcd424351c30268
SbDove <100377552+SbDove@users.noreply.github.com>
2024-06-17 10:07:01 -0700

    Restore file from main

--- test/end-to-end-testapp/build/compilation.js
+++ test/end-to-end-testapp/build/compilation.js
@@ -167,47 +167,39 @@ var HeapKit = (function (exports) {
 
     var initialization = {
         name: 'Heap',
-        /*  ****** Fill out initForwarder to load your SDK ******
-            Note that not all arguments may apply to your SDK initialization.
-            These are passed from mParticle, but leave them even if they are not being used.
-            forwarderSettings contain settings that your SDK requires in order to initialize
-            userAttributes example: {gender: 'male', age: 25}
-            userIdentities example: { 1: 'customerId', 2: 'facebookId', 7: 'emailid@email.com' }
-            additional identityTypes can be found at https://github.com/mParticle/mparticle-sdk-javascript/blob/master-v2/src/types.js#L88-L101
-        */
-        initForwarder: function (forwarderSettings, testMode, userAttributes, userIdentities, processEvent, eventQueue, isInitialized, common, appVersion, appName, customFlags, clientId) {
+    /*  ****** Fill out initForwarder to load your SDK ******
+        Note that not all arguments may apply to your SDK initialization.
+        These are passed from mParticle, but leave them even if they are not being used.
+        forwarderSettings contain settings that your SDK requires in order to initialize
+        userAttributes example: {gender: 'male', age: 25}
+        userIdentities example: { 1: 'customerId', 2: 'facebookId', 7: 'emailid@email.com' }
+        additional identityTypes can be found at https://github.com/mParticle/mparticle-sdk-javascript/blob/master-v2/src/types.js#L88-L101
+    */
+        initForwarder: function(forwarderSettings, testMode, userAttributes, userIdentities, processEvent, eventQueue, isInitialized, common, appVersion, appName, customFlags, clientId) {
             /* `forwarderSettings` contains your SDK specific settings such as apiKey that your customer needs in order to initialize your SDK properly */
             if (!testMode) {
                 /* Load your Web SDK here using a variant of your snippet from your readme that your customers would generally put into their <head> tags
                    Generally, our integrations create script tags and append them to the <head>. Please follow the following format as a guide:
                 */
 
+                if (!window.heap) {
+                    var heapScript = document.createElement('script');
+                    heapScript.type = 'text/javascript';
+                    heapScript.async = true;
+                    heapScript.src = 'https://cdn.heapanalytics.com/js/heap-' + forwarderSettings.appId + '.js';
+                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(heapScript);
+                    heapScript.onload = function() {
 
-                   if (!window.heap) {
-                    window.heap = window.heap || [];
-                    heap.load = function (e, t) {
-                        window.heap.appid = e, window.heap.config = t = t || {};
-                        var heapScript = document.createElement("script");
-                        heapScript.type = "text/javascript";
-                        heapScript.async = !0;
-                        heapScript.src = "https://cdn.heapanalytics.com/js/heap-" + e + ".js";
-                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(heapScript);
-
-                        heapScript.onload = function () {
-
-                            if (window.heap && eventQueue.length > 0) {
-                                // Process any events that may have been queued up while forwarder was being initialized.
-                                for (var i = 0; i < eventQueue.length; i++) {
-                                    processEvent(eventQueue[i]);
-                                }
-                                // now that each queued event is processed, we empty the eventQueue
-                                eventQueue = [];
+                        if (window.heap && eventQueue.length > 0) {
+                            // Process any events that may have been queued up while forwarder was being initialized.
+                            for (var i = 0; i < eventQueue.length; i++) {
+                                processEvent(eventQueue[i]);
                             }
-
-                        };
+                             // now that each queued event is processed, we empty the eventQueue
+                            eventQueue = [];
+                        }
+                       window.heap.load(forwarderSettings.appId);
                     };
-
-                    window.heap.load(forwarderSettings.appId);
                 }
 
 
@@ -399,7 +391,8 @@ var HeapKit = (function (exports) {
 
         function logSessionStart(event) {
             try {
-                return sessionHandler_1.onSessionStart(event);
+                sessionHandler_1.onSessionStart(event);
+                return true;
             } catch (e) {
                 return {
                     error: 'Error starting session on forwarder ' + name + '; ' + e,
@@ -409,7 +402,8 @@ var HeapKit = (function (exports) {
 
         function logSessionEnd(event) {
             try {
-                return sessionHandler_1.onSessionEnd(event);
+                sessionHandler_1.onSessionEnd(event);
+                return true;
             } catch (e) {
                 return {
                     error: 'Error ending session on forwarder ' + name + '; ' + e,
@@ -419,7 +413,8 @@ var HeapKit = (function (exports) {
 
         function logError(event) {
             try {
-                return self.eventHandler.logError(event);
+                self.eventHandler.logError(event);
+                return true;
             } catch (e) {
                 return {
                     error: 'Error logging error on forwarder ' + name + '; ' + e,
@@ -429,7 +424,8 @@ var HeapKit = (function (exports) {
 
         function logPageView(event) {
             try {
-                return self.eventHandler.logPageView(event);
+                self.eventHandler.logPageView(event);
+                return true;
             } catch (e) {
                 return {
                     error:
@@ -440,7 +436,8 @@ var HeapKit = (function (exports) {
 
         function logEvent(event) {
             try {
-                return self.eventHandler.logEvent(event);
+                self.eventHandler.logEvent(event);
+                return true;
             } catch (e) {
                 return {
                     error: 'Error logging event on forwarder ' + name + '; ' + e,
@@ -450,7 +447,8 @@ var HeapKit = (function (exports) {
 
         function logEcommerceEvent(event) {
             try {
-                return self.commerceHandler.logCommerceEvent(event);
+                self.commerceHandler.logCommerceEvent(event);
+                return true;
             } catch (e) {
                 return {
                     error:
@@ -736,12 +734,11 @@ var HeapKit = (function (exports) {
     }
 
     var SDKsettings = {
-        apiKey: 'TestApiKey',
+        apiKey: 'testAPIKey'
         /* fill in SDKsettings with any particular settings or options your sdk requires in order to
         initialize, this may be apiKey, projectId, primaryCustomerType, etc. These are passed
         into the src/initialization.js file as the
         */
-       appId: '1759220394'
     };
 
     // Do not edit below:
