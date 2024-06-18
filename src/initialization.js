22fa67cfa84932831af0a5b414fb9911c992023e
Steve Hwang <shwang@mparticle.com>
2024-06-17 21:24:47 -0500

    Check forwarder setting to send data s2s
    heap integration should forward s2s by default. do not init heap sdk unless settingtemplate value is true.

--- src/initialization.js
+++ src/initialization.js
@@ -27,38 +27,40 @@ var initialization = {
             /* Load your Web SDK here using a variant of your snippet from your readme that your customers would generally put into their <head> tags
                Generally, our integrations create script tags and append them to the <head>. Please follow the following format as a guide:
             */
+            common.forwardWebRequestsServerSide = forwarderSettings.forwardWebRequestsServerSide === 'True'
+            if (!forwarderSettings.forwardWebRequestsServerSide || forwarderSettings.forwardWebRequestsServerSide === "False") {
+                if (!window.heap) {
+                    window.heap = window.heap || [];
+                    heap.load = function (e, t) {
+                        (window.heap.appid = e), (window.heap.config = t = t || {});
+                        var heapScript = document.createElement('script');
+                        heapScript.type = 'text/javascript';
+                        heapScript.async = !0;
+                        heapScript.src =
+                            'https://cdn.heapanalytics.com/js/heap-' + e + '.js';
+                        (
+                            document.getElementsByTagName('head')[0] ||
+                            document.getElementsByTagName('body')[0]
+                        ).appendChild(heapScript);
 
-            if (!window.heap) {
-                window.heap = window.heap || [];
-                heap.load = function (e, t) {
-                    (window.heap.appid = e), (window.heap.config = t = t || {});
-                    var heapScript = document.createElement('script');
-                    heapScript.type = 'text/javascript';
-                    heapScript.async = !0;
-                    heapScript.src =
-                        'https://cdn.heapanalytics.com/js/heap-' + e + '.js';
-                    (
-                        document.getElementsByTagName('head')[0] ||
-                        document.getElementsByTagName('body')[0]
-                    ).appendChild(heapScript);
+                        heapScript.onload = function () {
+                            isInitialized = true;
 
-                    heapScript.onload = function () {
-                        isInitialized = true;
-
-                        if (window.heap && eventQueue.length > 0) {
-                            // Process any events that may have been queued up while forwarder was being initialized.
-                            for (var i = 0; i < eventQueue.length; i++) {
-                                processEvent(eventQueue[i]);
+                            if (window.heap && eventQueue.length > 0) {
+                                // Process any events that may have been queued up while forwarder was being initialized.
+                                for (var i = 0; i < eventQueue.length; i++) {
+                                    processEvent(eventQueue[i]);
+                                }
+                                // now that each queued event is processed, we empty the eventQueue
+                                eventQueue = [];
                             }
-                            // now that each queued event is processed, we empty the eventQueue
-                            eventQueue = [];
-                        }
+                        };
                     };
-                };
 
-                window.heap.load(forwarderSettings.appId);
-            } else {
-                isInitialized = true;
+                    window.heap.load(forwarderSettings.appId);
+                } else {
+                    isInitialized = true;
+                }
             }
         } else {
             // For testing, you should fill out this section in order to ensure any required initialization calls are made,
